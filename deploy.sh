4e2e931f3efd1c5a0aa458a072479f9c106491ea
Paul Walger <metaxy@walger.name>
2016-02-18 12:45:13 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
+git commit -am "deploy"
 gulp build
-
-rsync -az --force --delete --progress -e ssh ./dist/ ecg:free.ecg-berlin.de/
-rsync -az --force --delete --progress /home/paul/coding/f-free/data_benchmarks/ ecg:free.ecg-berlin.de/data_benchmarks/
-rsync -az --force --delete --progress -e ssh /home/paul/coding/f-free/model/ ecg:free.ecg-berlin.de/model/
+git checkout gh-pages
+git add dist/ -f
+rsync -az --force --delete --progress /home/paul/coding/f-free/data_benchmarks/ data_benchmarks/
+rsync -az --force --delete --progress -e ssh /home/paul/coding/f-free/model/ model/
+git add data_benchmarks/ -f
+git add model/ -f
+git commit -am "deploy"
+git push origin gh-pages
+git checkout master
 
 

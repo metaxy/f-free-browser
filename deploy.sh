bb7bcd255ed98904fe829d7dea73bb897c00be8c
Paul Walger <metaxy@walger.name>
2016-02-18 12:21:51 +0100

    ad

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 gulp build
+
 rsync -az --force --delete --progress -e ssh ./dist/ ecg:free.ecg-berlin.de/
-rsync -az --force --delete --progress -e ssh /home/paul/coding/f-free/data_benchmarks/ ecg:free.ecg-berlin.de/data_benchmarks/ 
-rsync -az --force --delete --progress -e ssh /home/paul/coding/f-free/model/ ecg:free.ecg-berlin.de/model/ 
+rsync -az --force --delete --progress /home/paul/coding/f-free/data_benchmarks/ ecg:free.ecg-berlin.de/data_benchmarks/
+rsync -az --force --delete --progress -e ssh /home/paul/coding/f-free/model/ ecg:free.ecg-berlin.de/model/
 
 
